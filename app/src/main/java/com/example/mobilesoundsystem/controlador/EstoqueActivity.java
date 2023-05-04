b0056eae294f572b518bd52308f1f733f26c18f7
Gustavo Gonçalves <gustavo.hgg@hotmail.com>
2023-05-04 20:44:30 -0300

    Atualização

---
+++ app/src/main/java/com/example/mobilesoundsystem/controlador/EstoqueActivity.java
@@ -0,0 +1,8 @@
+package com.example.mobilesoundsystem.controlador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EstoqueActivity extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/example/mobilesoundsystem/controlador/FuncionariosActivity.java
+++ app/src/main/java/com/example/mobilesoundsystem/controlador/FuncionariosActivity.java
@@ -14,18 +14,23 @@ public class FuncionariosActivity extends AppCompatActivity {
     Button btnCadastroGerente;
     Button btnCadastroVendedor;
 
+    Button btnVoltar;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funcionarios);
 
-
-
+        btnCadastroGerente = findViewById(R.id.btnCadastroGerente);
+        btnCadastroVendedor = findViewById(R.id.btnCadastroVendedor);
+        btnVoltar = findViewById(R.id.btnVoltar);
 
         btnCadastroGerente.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick (View vìew){
             Intent telaGerente = new Intent(getApplicationContext(), CadGerenteActivity.class);
             startActivity(telaGerente);
+
+
     }
     });
 
@@ -34,6 +39,13 @@ public class FuncionariosActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent telaVendedor = new Intent(getApplicationContext(), CadVendedorActivity.class);
                 startActivity(telaVendedor);
+            }
+        });
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 }
