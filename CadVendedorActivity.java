fe15d4586ea5ff958860a826e99ad15b355ca59a
Gustavo Gonçalves <49451806+Gustavohgg@users.noreply.github.com>
2023-09-21 11:11:32 -0300

    Add files via upload
    atualização

---
+++ CadVendedorActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobilesoundsystem.controlador;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobilesoundsystem.R;
+
+public class CadVendedorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrovendedor);
+    }
+}
